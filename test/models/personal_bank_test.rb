3cbc6893ac09d65420690d723b7263b545b0c7ae
Blake Strickland <Stricklandb02@gmail.com>
2016-03-02 20:25:03 -0500

    Add root route to dashboard

--- test/models/personal_bank_test.rb
+++ test/models/personal_bank_test.rb
@@ -7,4 +7,7 @@ class PersonalBankTest < ActiveSupport::TestCase
   test "total balance" do
     assert_equal 30.00, PersonalBank.balance
   end
+  test "total transactions" do
+      assert_equal 3, PersonalBank.num_of_trans
+  end
 end
