229339a04314bb73279caf8d102dc741e9ee419e
Blake Strickland <Stricklandb02@gmail.com>
2016-03-02 20:16:16 -0500

    Add balance method

--- app/models/personal_bank.rb
+++ app/models/personal_bank.rb
@@ -1,2 +1,5 @@
 class PersonalBank < ActiveRecord::Base
+  def self.balance
+    self.all.reduce(0.0) {|sum, x| sum + x.amount.to_f}
+  end
 end
--- test/models/personal_bank_test.rb
+++ test/models/personal_bank_test.rb
@@ -4,4 +4,7 @@ class PersonalBankTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  test "total balance" do
+    assert_equal 30.00, PersonalBank.balance
+  end
 end
