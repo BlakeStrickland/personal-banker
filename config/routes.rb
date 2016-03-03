9a26a5fac7203443f1240818e1757d5371e9940b
Blake Strickland <Stricklandb02@gmail.com>
2016-03-02 20:19:30 -0500

    Add root route to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'personal_banks#index'
   resources :personal_banks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
